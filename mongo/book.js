1d81c493fa14e57921b724383d5dd68ec244b565
vaibhavgoswami87 <vaibhavgoswami055@gmail.com>
2025-04-20 22:27:17 +0530

    added book.js

---
+++ mongo/book.js
@@ -0,0 +1,138 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+main().then(
+    (result) => {
+        console.log('Connected to MongoDB');
+    }
+).catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+//create a new book schema
+const BookSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        maxLength: 20,
+        minLength: 5,
+    },
+    author: {
+        type: String,
+    },
+    price: {
+        type: Number,
+
+    },
+    discount: {
+        type: Number,
+        default: 0,
+    },
+    category: {
+        type: String,
+        enum: ['fiction', 'non-fiction'],
+    },
+    genrics: {
+        type: [String],
+        default: [],
+    }
+});
+
+// create a new book model
+const Book = mongoose.model("Book", BookSchema);
+
+//create a new book1
+// const Book1 = new Book({
+//     title: "The Alchemist",
+//     author: "Paulo Coelho",
+//     price: 299,
+// })
+
+//create a new book2
+// const Book2 = new Book({
+//     title: "The Monk Who Sold His Ferrari",
+//     author: "Robin Sharma",
+//     price: 399,
+// })
+
+//book1 save
+// Book1.save().then(() => {
+//     console.log("Book 1 saved");
+// }).catch(err => {
+//     console.log("Error saving book 1:", err);
+// })
+
+//book2 save
+// Book2.save().then(() => {
+//     console.log("Book 2 saved");
+// }).catch(err => {
+//     console.log("Error saving book 2:", err);
+// })
+
+
+// minlength  validation
+// const Book3 = new Book({
+//     title: "he",
+//     author: "dnwj",
+//     price: 200,
+// })
+
+// Book3.save().then(() => {
+//     console.log("Book 3 saved");
+// }).catch(err => {
+//     console.log("Error saving book 3:", err);
+// })
+
+// maxLength validation
+// const Book4 = new Book({
+//     title: "The Alchemist is a great book",
+//     author: "Paulo Coelho",
+//     price: 299,
+// })
+// Book4.save().then(() => {
+//     console.log("Book 4 saved");
+// }).catch(err => {
+//     console.log("Error saving book 4:", err);
+// })
+
+// const Book5 = new Book({
+//     title: "Marvel Comics",
+//     author: "Stan Lee",
+//     price: 499,
+//     category: "fiction",
+// })
+
+// Book5.save().then(() => {
+//     console.log("Book 5 saved");
+// }).catch(err => {
+//     console.log("Error saving book 5:", err);
+// })
+
+// const Book6 = new Book({
+//     title: "Marvel Comics",
+//     author: "Stan Lee",
+//     price: 499,
+//     category: "ds",
+// })
+
+// Book6.save().then(() => {
+//     console.log("Book 6 saved");
+// }).catch(err => {
+//     console.log("Error saving book 5:", err);
+// })
+
+const Book7 = new Book({
+    title: "Marvel Ceefe",
+    author: "Stan dsdLee",
+    price: 1499,
+    category: "fiction",
+    genrics: ["action", "adventure"],
+})
+Book7.save().then(() => {
+    console.log("Book 7 saved");
+}).catch(err => {
+    console.log("Error saving book 7:", err);
+})
--- mongo/index.js
+++ mongo/index.js
@@ -21,11 +21,11 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 
-User.insertMany([
-    { name: 'Vaibhav Pari', email: 'vaibhavpari@gmail.com', age: 20 },
-    { name: 'Manan', email: 'manan@gmail.com', age: 25 },
-    { name: 'Rajat', email: 'rajat@gmail.com', age: 30 },
-])
+// User.insertMany([
+//     { name: 'Vaibhav Pari', email: 'vaibhavpari@gmail.com', age: 20 },
+//     { name: 'Manan', email: 'manan@gmail.com', age: 25 },
+//     { name: 'Rajat', email: 'rajat@gmail.com', age: 30 },
+// ])
 
 // const user1 = new User({
 //     name: 'John Doe',
@@ -46,3 +46,43 @@ User.insertMany([
 // })
 
 // user2.save();
+
+/// find all users
+// User.find().then((users) => {
+//     console.log(users);
+// }).catch(err => {
+//     console.log('Error finding users:', err);
+// })
+
+// find user by age
+// User.find({ age: { $gt: 30 } }).then((users) => {
+//     console.log(users);
+// })
+
+// multiple user data update
+// User.updateMany({ age: 20 }, { age: 21 }).then((result) => {
+//     console.log(result);
+// }).catch(err => {
+//     console.log('Error updating users:', err);
+// })
+
+// findOneAndUpdate example
+// User.findOneAndUpdate({ name: 'Vaibhav' }, { age: 22 }, { new: true }).then((user) => {
+//     console.log(user);
+// }).catch(err => {
+//     console.log('Error updating user:', err);
+// })
+
+//Delete Example
+// User.deleteOne({ name: "Rajat" }).then((result) => {
+//     console.log(result);
+// }).catch(err => {
+//     console.log('Error deleting user:', err);
+// })
+
+// Delete Many Example
+// User.deleteMany({ age: { $gt: 22 } }).then((result) => {
+//     console.log(result);
+// }).catch(err => {
+//     console.log('Error deleting users:', err);
+// })
